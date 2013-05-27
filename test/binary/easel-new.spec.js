1389f9b4c483dd22229854891fcac3ee20b6cb14
Jeremy Worboys <jw@jeremyworboys.com>
2013-05-27 13:46:14 +1000

    Generate a new application

---
+++ test/binary/easel-new.spec.js
@@ -0,0 +1,95 @@
+
+var exec = require('child_process').exec;
+var fs = require('fs');
+var wrench = require('wrench');
+
+
+describe('easel', function() {
+
+    var easelBin = './bin/easel';
+
+    describe('new <name>', function() {
+
+        var appName = 'testApp';
+
+        it('should create a new application', function(done) {
+            exec(easelBin + ' new ' + appName, function(err, stdout) {
+                if (err) return done(err);
+                stdout.should.include('Creating');
+                checkScaffoldNew(appName).should.be.ok;
+                done();
+            });
+        });
+
+        beforeEach(function(done) {
+            fs.exists(appName, function(exists) {
+                if (exists) {
+                    wrench.rmdirSyncRecursive(appName);
+                }
+                done();
+            });
+        });
+
+        afterEach(function(done) {
+            fs.exists(appName, function(exists) {
+                if (exists) {
+                    wrench.rmdirSyncRecursive(appName);
+                }
+                done();
+            });
+        });
+    });
+});
+
+
+function checkScaffoldNew(appName) {
+    var expectedFiles = JSON.stringify([
+        'app',
+        'app/assets',
+        'app/assets/javascripts',
+        'app/assets/javascripts/application.js',
+        'app/assets/javascripts/vendor',
+        'app/assets/javascripts/vendor/.gitkeep',
+        'app/assets/stylesheets',
+        'app/assets/stylesheets/application.css.styl',
+        'app/controllers',
+        'app/controllers/application_controller.js',
+        'app/models',
+        'app/models/.gitkeep',
+        'app/views',
+        'app/views/application',
+        'app/views/application/index.html.hbs',
+        'app/views/layouts',
+        'app/views/layouts/application.hbs',
+        'config',
+        'config/application.js',
+        'config/environments',
+        'config/environments/all.js',
+        'config/environments/development.js',
+        'config/environments/production.js',
+        'config/environments/test.js',
+        'config/initializers',
+        'config/initializers/session.js',
+        'config/routes.js',
+        'public',
+        'public/favicon.ico',
+        'public/robots.txt',
+        'test',
+        'test/.jshintrc',
+        'test/mocha.opts',
+        '.editorconfig',
+        '.gitattributes',
+        '.gitignore',
+        '.jshintrc',
+        '.npmignore',
+        'index.js',
+        'LICENSE',
+        'Makefile',
+        'package.json',
+        'README.md'
+    ].sort());
+
+    var files = JSON.stringify(wrench.readdirSyncRecursive(appName).sort());
+
+    return files === expectedFiles;
+}
