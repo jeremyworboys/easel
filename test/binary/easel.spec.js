aae3087afdc7bccecd2da144c582076627a276d3
Jeremy Worboys <jw@jeremyworboys.com>
2013-05-27 10:58:40 +1000

    Clean up test

--- test/binary/easel.spec.js
+++ test/binary/easel.spec.js
@@ -3,8 +3,11 @@ var exec = require('child_process').exec;
 
 
 describe('easel', function() {
+
+    var easelBin = './bin/easel';
+
     it('should output help', function(done) {
-        exec('bin/easel', function(err, stdout) {
+        exec(easelBin, function(err, stdout) {
             if (err) return done(err);
             stdout.should.include('easel <command> [options]');
             stdout.should.include('--help');
